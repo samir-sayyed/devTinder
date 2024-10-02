621a62cfe3dbef5b278fc586d07c589f4c0b38f9
samir-sayyed <sayyedsamir11@gmail.com>
2024-10-02 22:06:07 +0530

    Added crud apis

--- src/app.js
+++ src/app.js
@@ -6,6 +6,47 @@ const User = require("./models/user")
 
 app.use(express.json())
 
+app.get("/user", async (req, res) => {
+    try{
+        const userEmail = req.body.emailId
+        var user = await User.find({emailId: userEmail})
+        res.status(200).send(user)
+    }catch(err){
+        res.status(404).send("Something went wrong")
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({})
+        res.status(200).send(users)
+    }catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.delete("/user", async (req, res) =>{
+    try{
+        const userId = req.body.userId
+        const user = await User.findByIdAndDelete(userId)
+        console.log(user)
+        res.send("User deleted successfully")
+    }catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.patch("/user", async (req, res) => {
+    try{
+        const userId = req.body.userId;
+        const data = req.body
+        const user = await User.findByIdAndUpdate(userId, data)
+        res.status(200).send(user)
+    }catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+
 app.post("/signup", async (req, res) =>{
    try{
         const user = new User(req.body);
